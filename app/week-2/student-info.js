68daf5e07fabed019cc3e24dc409092536667cff
Nikita Konakov <nik.konakov@gmail.com>
2024-05-24 21:28:16 -0600

    First Commit to Repo

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+    return (
+      <main>
+        <ul><b>Student Name:</b></ul>
+        <li>Nikita Konakov</li>
+        <li>Hey, check my <b><a href="https://github.com/NikKonakov">GitHub Account!</a></b></li>
+        <p>P.S. "Github Account!" is clickable, but if you don't find it here is link: <a href="https://github.com/NikKonakov">https://github.com/NikKonakov</a> </p>
+      </main>
+    );
+  }
